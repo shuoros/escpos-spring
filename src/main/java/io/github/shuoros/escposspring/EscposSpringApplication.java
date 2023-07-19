6d13f4bb9cb08f3c06b703839438f7624bb81ebc
Soroush <shuoros@yahoo.com>
2023-07-19 17:30:24 +0200

    INIT[Spring Boot]

---
+++ src/main/java/io/github/shuoros/escposspring/EscposSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.escposspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscposSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscposSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/shuoros/escposspring/EscposSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.escposspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscposSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
